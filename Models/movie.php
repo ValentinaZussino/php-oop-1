c65202ce8d42c883ae703af1bbc3598e1150c9f3
ValentinaZussino <vzussino@protonmail.com>
2022-12-12 17:41:38 +0100

    agg public function

--- Models/movie.php
+++ Models/movie.php
@@ -2,18 +2,32 @@
 
 class Movie {
     // variabili
-    public $title;
-    public $genres;
-    public $length;
-    public $year;
-    public $director;
+    private $title;
+    private $genres;
+    private $length;
+    private $year;
+    private $director;
+    private $details;
+    private $description;
 
     // constructor
-    function __construct(string $_title, string $_year, array $_genres, string $_director){
+    function __construct(string $_title, string $_year, array $_genres, string $_length, string $_director, string $_details = null){
         $this->title = $_title;
         $this->genres = $_genres;
+        $this->length = $_length;
         $this->year = $_year;
         $this->director = $_director;
+        $this->details = $_details;
+
+        $this->setDescription();
+    }
+    // funzione x descrizione
+    public function setDescription(){
+        if($this->details == null){
+            return $this->description = 'Al momento non sono presenti ulteriori dettagli';
+        } else {
+            $this->description = $this->details;
+        }
     }
 }
 
