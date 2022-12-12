731e40f822ec102f8e44f6971b4f6e71ce1d11e0
ValentinaZussino <vzussino@protonmail.com>
2022-12-12 15:51:17 +0100

    variabili + costruttore

--- Models/movie.php
+++ Models/movie.php
@@ -1,6 +1,21 @@
 <?php
 
+class Movie {
+    // variabili
+    public $title;
+    public $genres;
+    public $length;
+    public $year;
+    public $director;
 
+    // constructor
+    function __construct(string $_title, string $_year, array $_genres, string $_director){
+        $this->title = $_title;
+        $this->genres = $_genres;
+        $this->year = $_year;
+        $this->director = $_director;
+    }
+}
 
 
 ?>
--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
 include_once __DIR__ . '/Models/movie.php';
+// verifico funzionamenti
+// echo ('ciao');
+// $prova = new Movie();
+// $prova->title = "titolo di prova";
+// var_dump($prova);
 
+// stampa film
+$terminator = new Movie("Terminator", "1984", ["action", "fantasy", "thriller"], "James Cameron");
+$terminator2 = new Movie("Terminator 2: Judgment Day", "1991", ["action", "fantasy", "thriller"], "James Cameron");
 
+var_dump($terminator);
+var_dump($terminator2);
 
 ?>
